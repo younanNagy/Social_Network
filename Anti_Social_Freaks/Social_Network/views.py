9a8ed0b7d86a9bb3a9059de842d97c8b22848065
youssephAhmed <yousseffahmed@hotmail.com>
2018-04-30 20:52:42 +0200

    adding templates

--- Anti_Social_Freaks/Social_Network/views.py
+++ Anti_Social_Freaks/Social_Network/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django .template import loader
+
+
 
 # Create your views here.
 
 def home(request): # this is just a test make sure to try this path first to make sure that everyting is ok
-    return HttpResponse("<h1> Home Page")
+    return render(request,'Social_Network/test.html')
